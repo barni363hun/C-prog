8ff59e6ac22206265e69ec4a4cc4910bb875f267
barni363hun <barni.pbs@gmail.com>
2022-11-11 20:23:09 +0000

    c

--- src/szt_alap_beadando/get_put.c
+++ src/szt_alap_beadando/get_put.c
@@ -2,51 +2,36 @@
 #include <stdlib.h>
 
 int main() {
-  // open the file
-  FILE *fp = fopen("test.txt", "r");
+  // open the files
+
+  FILE *ofp = fopen("output.txt", "w");
 
   // Return if could not open file
-  if (fp == NULL)
+  if (ofp == NULL)
     return 0;
+
+  FILE *ifp = fopen("input.txt", "r");
+  int file_end = 0;
+  // Return if could not open file
+  if (ifp == NULL) {
+    printf("input.txt not found!");
+    return 0;
+  }
 
   do {
-    // Taking input single character at a time
-    char c = fgetc(fp);
-
     // Checking for end of file
-    if (feof(fp))
-      break;
+    if (!feof(ifp)) {
+      // Taking input single character at a time
+      char c = fgetc(ifp);
+      fputc(c, ofp);
+    } else {
+      file_end = 1;
+    }
+    // printf("%c", c);
+  } while (file_end != 1);
 
-    printf("%c", c);
-  } while (1);
+  fclose(ifp);
+  fclose(ofp);
 
-  fclose(fp);
-  return (0);
-  int i = 0;
-
-  
-  FILE *fp = fopen("output.txt", "w");
-
-  // Return if could not open file
-  if (fp == NULL)
-    return 0;
-
-  char string[] = "good bye", received_string[20];
-
-  for (i = 0; string[i] != '\0'; i++)
-
-    // Input string into the file
-    // single character at a time
-    fputc(string[i], fp);
-
-  fclose(fp);
-  fp = fopen("output.txt", "r");
-
-  // Reading the string from file
-  fgets(received_string, 20, fp);
-
-  printf("%s", received_string);
-
-  fclose(fp);
   return 0;
 }
